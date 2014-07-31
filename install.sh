6d92d77413177e637b37c9381999c7573186b5fe
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2014-07-31 09:14:37 -0500

    update install script. Mac only

--- install.sh
+++ install.sh
@@ -2,23 +2,16 @@
 
 set -x
 
-if [ -e ~/.vimrc ]; then
-  echo "~/.vimrc already exists"
-  exit;
-else
-  cp -p vimrc.loader ~/.vimrc
-fi
-
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   # ...
   echo "linux-gnu"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "Installing vimrc for Mac"
-  if [ -h ~/.vimrc ]; then  # file is a symbolic link
-    echo "~/.vimrc is symlink, not appending host specific source file."
-  else 
-    echo "source ~/Dropbox/dotfiles/vim/vimrc.Mac" >> ~/.vimrc 
+  # TODO: parameterize for any location of source file 
+  if [ -e ~/.vimrc ];
+  then
+    mv ~/.vimrc ~/vimrc.$RANDOM
   fi
+  cp -p vimrc.Mac.local ~/.vimrc
 
 elif [[ "$OSTYPE" == "cygwin" ]]; then
   # ...
