7f6a51dddf2f38fcb8ed21a0c0e476de4615e8ec
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2014-08-04 23:42:47 -0500

    exrc ignored, has compatible not working

--- install.sh
+++ install.sh
@@ -10,6 +10,12 @@ fi
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   # TODO: parameterize for any location of source file 
   cp -p vimrc.SL.local ~/.vimrc
+  if [ -e ~/.exrc ];
+  then
+    mv ~/.exrc ~/exrc.$RANDOM
+  fi
+  cp -p exrc ~/.exrc
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   # TODO: parameterize for any location of source file 
   cp -p vimrc.Mac.local ~/.vimrc
---
+++ vimrc.exrc
@@ -0,0 +1,4 @@
+if !has("compatible")
+  set nu ai ts=2 sw=2
+  syntax on
+endif
